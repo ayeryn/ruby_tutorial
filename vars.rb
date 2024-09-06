5b6e31a1e3025bf371ad4976cc60b9b87fb65ccf
ayeryn <erynli@utexas.edu>
2024-09-06 15:38:10 -0500

    string

--- vars.rb
+++ vars.rb
@@ -5,4 +5,18 @@ character_age = "30"
 puts ("My name is " + character_name)
 puts ("I'm turning " + character_age + " this year")
 puts ("I started going by " + character_name + " when I started college")
-puts ("I don't feel like " + character_age +  " at all")
+puts ("I don't feel like " + character_age +  " at all\n\n")
+
+s = "AbCdEfG"
+puts ("Here are some string methods! Given a string " + s)
+puts "The uppercase is s.upcase(): " + s.upcase()
+puts "The lowercase is s.downcase(): " + s.downcase()
+puts "We can strip leading and trailing ws with s.strip()"
+print "The length of s is s.length(): " 
+print s.length()
+puts ""
+print "Does s include 'abc'? "
+print s.include?"abc"
+puts ""
+print "Does s include 'bCd'? "
+print s.include?"bCd"
