b1d18bdb96dbe85fb896eee4624aad4095b9a6d0
ayeryn <erynli@utexas.edu>
2024-09-06 12:31:29 -0500

    vars

--- vars.rb
+++ vars.rb
@@ -1,4 +1,8 @@
-puts "My name is Eryn"
-puts "I'm turning 30 in September"
-puts "I started going by Eryn when I started college"
-puts "I don't feel like 30 at all"
+character_name = "Eryn"
+character_age = "30"
+# character_age = 30
+
+puts ("My name is " + character_name)
+puts ("I'm turning " + character_age + " this year")
+puts ("I started going by " + character_name + " when I started college")
+puts ("I don't feel like " + character_age +  " at all")
