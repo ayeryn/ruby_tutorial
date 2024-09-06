3f46882614c4f9eecc340db06d155636625095a9
ayeryn <erynli@utexas.edu>
2024-09-06 12:25:20 -0500

    puts and print

---
+++ test.rb
@@ -0,0 +1,13 @@
+# Adds new line at EOL
+puts "Eryn"
+
+# No new line at EOL
+print "Hello World"
+print ", Howdy?\n\n"
+
+# Draws a triangle
+puts "Here is a triangle!"
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
